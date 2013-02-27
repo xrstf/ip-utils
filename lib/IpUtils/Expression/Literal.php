3acc565a2cf83ca74b582d20d8a634172f310e9d
Christoph <christoph@webvariants.de>
2013-02-27 03:19:47 +0100

    be more strict about literals, so we can compare a normalized version

--- lib/IpUtils/Expression/Literal.php
+++ lib/IpUtils/Expression/Literal.php
@@ -11,12 +11,27 @@
 namespace IpUtils\Expression;
 
 use IpUtils\Address\AddressInterface;
+use IpUtils\Address\IPv4;
+use IpUtils\Address\IPv6;
+use IpUtils\Exception\InvalidExpressionException;
 
 class Literal implements ExpressionInterface {
 	protected $expression;
 
 	public function __construct($expression) {
-		$this->expression = strtolower(trim($expression));
+		$expression = strtolower(trim($expression));
+
+		if (IPv4::isValid($expression)) {
+			$ip = new IPv4($expression);
+		}
+		elseif (IPv6::isValid($expression)) {
+			$ip = new IPv6($expression);
+		}
+		else {
+			throw new InvalidExpressionException('Expression must be either a valid IPv4 or IPv6 address.');
+		}
+
+		$this->expression = $ip->getCompact();
 	}
 
 	/**
@@ -26,6 +41,6 @@ class Literal implements ExpressionInterface {
 	 * @return boolean
 	 */
 	public function matches(AddressInterface $address) {
-		return $address->getExpanded() === $this->expression;
+		return $address->getCompact() === $this->expression;
 	}
 }
